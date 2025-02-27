ae78e134ff2072e44523b0333af5ab128836936f
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2025-02-27 20:16:09 +0530

    vercel and gitignore and envlocal

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:5173", // Vite's default port
+		origin: process.env.CORS_ORIGIN || "http://localhost:5173",
 		credentials: true,
 	})
 );
